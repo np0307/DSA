e3740157bc2aa4cffe153736bf1584135a9c00fa
np0307 <neha0307work@gmail.com>
2025-08-08 16:23:50 +0530

    Time: 556 ms (60.54%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,18 +1,10 @@
 SELECT 
     ROUND(
-        COUNT(DISTINCT CASE 
-            WHEN EXISTS (
-                SELECT 1 
-                FROM Activity a2 
-                WHERE a2.player_id = a1.player_id 
-                AND a2.event_date = DATE_ADD(a1.first_login, INTERVAL 1 DAY)
-            ) 
-            THEN a1.player_id 
-            END
-        ) * 1.0 / COUNT(*), 2
+        SUM(CASE WHEN a2.player_id IS NOT NULL THEN 1 ELSE 0 END) / COUNT(DISTINCT a1.player_id), 
+        2
     ) AS fraction
-FROM (
-    SELECT player_id, MIN(event_date) AS first_login
-    FROM Activity
-    GROUP BY player_id
-) a1;
+FROM
+    (SELECT player_id, MIN(event_date) AS first_login_date FROM Activity GROUP BY player_id) a1
+LEFT JOIN 
+    Activity a2
+ON a1.player_id = a2.player_id AND a2.event_date = DATE_ADD(a1.first_login_date, INTERVAL 1 DAY);
