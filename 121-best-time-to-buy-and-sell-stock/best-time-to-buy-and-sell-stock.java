81ab513046cc765bc9557e48218c54ae28ea28e9
np0307 <neha0307work@gmail.com>
2025-12-12 00:15:06 +0530

    Time: 2 ms (53.80%) | Memory: 94.5 MB (14.67%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,6 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
-       
+        if(prices==null || prices.length<2)
+        {
+            return 0;
+        }
 
         int minPrice=prices[0];
         int maxProfit = 0;
